54843a478be831f856aee7de091c61c8315b8187
ckickendev <thesoonafu@gmail.com>
2025-03-30 22:48:30 +0800

    near done authen

--- src/component/authen/AuthenPage.tsx
+++ src/component/authen/AuthenPage.tsx
@@ -6,7 +6,7 @@ import LoadingComponent from "../common/Loading";
 import ConfirmSignUp from "./ConfirmSignUp";
 
 export const AuthenPage = ({ setIsAuthenFromDisplay }: DataAuthen) => {
-    const [authenState, setAuthenState] = useState(1);
+    const [authenState, setAuthenState] = useState(3);
     const [isLoading, setIsLoading] = useState(false);
 
     const renderCore = () => {
@@ -28,7 +28,7 @@ export const AuthenPage = ({ setIsAuthenFromDisplay }: DataAuthen) => {
 
     return (
         <div className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex justify-center items-center bg-gray-50/50">
-            <div className='relative p-4 w-full max-w-md max-h-full bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+            <div className='relative p-4 w-full max-w-md max-h-full bg-gray-100 flex flex-col items-center justify-center py-12 sm:px-6 lg:px-8'>
                 <LoadingComponent isLoading={isLoading} />
                 {renderCore()}
             </div>
--- src/component/authen/ConfirmSignUp.tsx
+++ src/component/authen/ConfirmSignUp.tsx
@@ -2,7 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { loginSchema, REACT_APP_ROOT_BACKEND } from "./authentication";
 import axios from "axios";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 
 
@@ -13,27 +13,102 @@ type ModalAuthen = {
 };
 
 const ConfirmSignUp = ({ setAuthenState, setIsLoading, setIsAuthenFromDisplay }: ModalAuthen) => {
+    const inputRef1 = useRef(null);
+    const inputRef2 = useRef(null);
+    const inputRef3 = useRef(null);
+    const inputRef4 = useRef(null);
+    const inputRef5 = useRef(null);
+    const inputRef6 = useRef(null);
+    const submit = useRef(null);
     const [errorSv, setErrorSv] = useState("");
 
+    const onSubmit = async (event: any) => {
+        event.preventDefault();
+        setIsLoading(true);
 
-    // const onSubmit = async (data: TokenConfirm, event: any) => {
-    //     event.preventDefault();
-    //     setIsLoading(true);
-    //     console.log(data);
-    //     event.preventDefault();
-    //     try {
-    //         const res = await axios.post(`${REACT_APP_ROOT_BACKEND}/auth/login`, data);
-    //         if (res.data) {
-    //             localStorage.setItem("access_token", res.data.data.access_token);
-    //             setIsLoading(false);
-    //         }
-    //         return;
-    //     }
-    //     catch (err: any) {
-    //         setIsLoading(false);
-    //         setErrorSv(err?.response?.data?.error || err?.message);
-    //     }
-    // };
+        const token = inputRef1.current.value + inputRef2.current.value + inputRef3.current.value
+            + inputRef4.current.value + inputRef5.current.value + inputRef6.current.value;
+        if (!new RegExp(`^[0-9]{${6}}$`).test(token)) {
+            setErrorSv("Your token is not valid format, please enter again");
+            setIsLoading(false);
+            return;
+        }
+
+        try {
+            const loginInfo = {
+                token,
+                user_authen: localStorage.getItem("user_signup"),
+            };
+            console.log(loginInfo);
+
+            const res = await axios.post(
+                `${REACT_APP_ROOT_BACKEND}/auth/confirmSignup`,
+                loginInfo
+            );
+            setErrorSv(res.data.message);
+            if (res.status === 200) {
+                setIsLoading(false);
+                localStorage.removeItem("user_signup");
+                setIsLoading(true);
+                setTimeout(() => {
+
+                    setAuthenState(1);
+                    setIsLoading(false);
+                }, 5000);
+            }
+        }
+        catch (err: any) {
+            setIsLoading(false);
+            setErrorSv(err?.response?.data?.error || err?.message);
+        }
+    };
+
+    const handleInput = (e: any, index: number) => {
+        setErrorSv("");
+        const { target } = e
+        if (target.value) {
+            if (index < 6) {
+                switch (index) {
+                    case 1:
+                        inputRef2.current.focus();
+                        break
+                    case 2:
+                        inputRef3.current.focus();
+                        break;
+                    case 3:
+                        inputRef4.current.focus();
+                        break;
+                    case 4:
+                        inputRef5.current.focus();
+                        break;
+                    case 5:
+                        inputRef6.current.focus();
+                        break;
+                }
+            } else {
+                submit.current.focus();
+            }
+        }
+    }
+
+    const handlePaste = (e: any) => {
+        e.preventDefault();
+        setErrorSv("");
+        const text = e.clipboardData.getData('text')
+        if (!new RegExp(`^[0-9]{${6}}$`).test(text)) {
+            setErrorSv("Cannot paste, invalid data")
+            return
+        }
+        const digits = text.split('')
+        inputRef1.current.value = digits[0];
+        inputRef2.current.value = digits[1];
+        inputRef3.current.value = digits[2];
+        inputRef4.current.value = digits[3];
+        inputRef5.current.value = digits[4];
+        inputRef6.current.value = digits[5];
+
+        submit.current.focus()
+    }
 
     return <>
         <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center cursor-pointer dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal" onClick={() => setIsAuthenFromDisplay(false)}>
@@ -47,58 +122,73 @@ const ConfirmSignUp = ({ setAuthenState, setIsLoading, setIsAuthenFromDisplay }:
                 <img className="w-[152px] h-[100px]" src="/logo.png" alt="open-trash" />
             </div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sign in to your account
+                Confirm your email
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600 max-w">
-                Or
+                Check your email and enter code here
+            </p>
+            <p className="mt-2 text-center text-sm text-gray-600 max-w">
+                Or you can skip
                 <span onClick={() => setAuthenState(2)} className="font-medium text-blue-600 hover:text-blue-500 cursor-pointer">
-                    &nbsp; create an account
+                    &nbsp; right now
                 </span>
             </p>
         </div>
-
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-
-                <div className="mt-6">
-
-                    <div className="relative">
-                        <div className="absolute inset-0 flex items-center">
-                            <div className="w-full border-t border-gray-300"></div>
-                        </div>
-                        <div className="relative flex justify-center text-sm">
-                            <span className="px-2 bg-gray-100 text-gray-500">
-                                Or continue with
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="mt-6 grid grid-cols-3 gap-3">
-                        <div className="h-full">
-                            <a href="#"
-                                className="h-full w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                <img className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg"
-                                    alt="" />
-                            </a>
-                        </div>
-                        <div className="h-full">
-                            <a href="#"
-                                className="h-full w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg"
-                                    alt="" />
-                            </a>
-                        </div>
-                        <div className="h-full">
-                            <a href="#"
-                                className="h-full w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                <img className="h-6 w-6" src="https://www.svgrepo.com/show/506498/google.svg"
-                                    alt="" />
-                            </a>
-                        </div>
-                    </div>
+        {/* text-center px-4 sm:px-8 py-10 rounded-xl */}
+        <div className="max-w-md mx-auto text-center py-10 rounded-xl">
+            <form id="otp-form">
+                <div className="flex items-center justify-center gap-2">
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        pattern="\d*" maxLength={1}
+                        onInput={(e) => handleInput(e, 1)}
+                        onPaste={handlePaste}
+                        ref={inputRef1} />
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        maxLength={1}
+                        onInput={(e) => handleInput(e, 2)}
+                        ref={inputRef2} />
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        maxLength={1}
+                        onInput={(e) => handleInput(e, 3)}
+                        ref={inputRef3} />
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        maxLength={1}
+                        onInput={(e) => handleInput(e, 4)}
+                        ref={inputRef4} />
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        maxLength={1}
+                        onInput={(e) => handleInput(e, 5)}
+                        ref={inputRef5} />
+                    <input
+                        type="text"
+                        className="w-14 h-14 text-center text-2xl font-extrabold text-slate-900 bg-white border border-transparent hover:border-slate-200 appearance-none rounded p-4 outline-none focus:bg-white focus:border-indigo-400 focus:ring-2 focus:ring-indigo-100"
+                        maxLength={1}
+                        onInput={(e) => handleInput(e, 6)}
+                        ref={inputRef6} />
                 </div>
-            </div>
+                {errorSv && <p className="text-red-500">{errorSv}</p>}
+
+                <div className="max-w-[260px] mx-auto mt-4">
+                    <button type="submit"
+                        onClick={onSubmit}
+                        ref={submit}
+                        className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:bg-red focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150">Verify
+                        Account</button>
+                </div>
+            </form>
+            <div className="text-sm text-slate-500 mt-4">Didn't receive code? <a className="font-medium text-indigo-500 hover:text-indigo-600" href="#0">Resend</a></div>
         </div>
+
     </>
 }
 
