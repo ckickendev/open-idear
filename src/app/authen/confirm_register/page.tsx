87591669b674998d2aa3d803e98055a85e65a41e
ckickendev <thesoonafu@gmail.com>
2025-04-04 02:36:42 +0800

    reset passlink

---
+++ src/app/authen/confirm_register/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import LoadingComponent from "@/component/common/Loading";
+import Dialog from "@/component/common/Dialog";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { REACT_APP_ROOT_BACKEND } from "@/component/authen/authentication";
+
+const ConfirmSignUpByLink = () => {
+    const [isConfirm, setIsConfirm] = useState(false);
+    const [title, setTittle] = useState("");
+    const [content, setContent] = useState("");
+    const [confirmTitle, setConfirmTitle] = useState("");
+
+    useEffect(() => {
+        async function handleConfirm() {
+            const queryParameters = new URLSearchParams(window.location.search);
+            const access_token = queryParameters.get("access_token");
+            const email = queryParameters.get("email");
+            
+            const loginInfo = {
+                token: access_token,
+                user_authen: email,
+            };
+            console.log("loginInfo", loginInfo);
+
+            try {
+                if (access_token && email) {
+                    const res = await axios.post(
+                        `${REACT_APP_ROOT_BACKEND}/auth/confirmSignup`,
+                        loginInfo
+                    );
+                    if (res.data) {
+                        setTittle(res.data.message);
+                        setContent(
+                            "You can login right now, you will redirect to login in some minutes..."
+                        );
+                        setIsConfirm(true);
+                        setConfirmTitle("Go to home now");
+                        setTimeout(() => {
+                            redirect("/");
+                        }, 3000);
+                    }
+                }
+            } catch (err: any) {
+                setContent("Please check again");
+                setConfirmTitle("I agree");
+                setIsConfirm(true);
+                setTittle(err?.response?.data?.error || err.message);
+            }
+        }
+        handleConfirm();
+    });
+
+    const confirmToHomePage = () => {
+        redirect('/')
+    };
+
+    return (
+        <>
+            <LoadingComponent isLoading={!isConfirm} />
+            {isConfirm && (
+                <Dialog
+                    onClose={() => {
+                        setIsConfirm(false);
+                    }}
+                    isOpen={isConfirm}
+                    title={title}
+                    message={content}
+                    confirmTitle={confirmTitle}
+                    confirmAction={confirmToHomePage}
+                />
+            )}
+        </>
+    );
+};
+
+export default ConfirmSignUpByLink;
--- src/app/authen/reset-password-link/page.tsx
+++ src/app/authen/reset-password-link/page.tsx
@@ -1,57 +1,68 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import LoadingComponent from "../../component/common/Loading";
-import Dialog from "../../component/common/Dialog";
+import LoadingComponent from "@/component/common/Loading";
+import Dialog from "@/component/common/Dialog";
 import axios from "axios";
 import { redirect } from "next/navigation";
 import { REACT_APP_ROOT_BACKEND } from "@/component/authen/authentication";
 
-const ConfirmSignUpByLink = () => {
-    const [isConfirm, setIsConfirm] = useState(false);
+const ConfirmResetPassword = () => {
+    const [isModalConfirmDisp, setIsModalConfirmDisp] = useState(false);
+    const [formResetDisp, setFormResetDisp] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [title, setTittle] = useState("");
     const [content, setContent] = useState("");
     const [confirmTitle, setConfirmTitle] = useState("");
 
     useEffect(() => {
         async function handleConfirm() {
+            setIsLoading(true);
             const queryParameters = new URLSearchParams(window.location.search);
-            const access_token = queryParameters.get("access_token");
+            const access_token = queryParameters.get("token_access");
             const email = queryParameters.get("email");
-            
+
             const loginInfo = {
-                token: access_token,
-                user_authen: email,
+                access_token,
+                email,
             };
             console.log("loginInfo", loginInfo);
 
             try {
                 if (access_token && email) {
                     const res = await axios.post(
-                        `${REACT_APP_ROOT_BACKEND}/auth/confirmSignup`,
+                        `${REACT_APP_ROOT_BACKEND}/auth/confirm-token-access`,
                         loginInfo
                     );
                     if (res.data) {
-                        setTittle(res.data.message);
-                        setContent(
-                            "You can login right now, you will redirect to login in some minutes..."
-                        );
-                        setIsConfirm(true);
-                        setConfirmTitle("Go to home now");
-                        setTimeout(() => {
-                            redirect("/");
-                        }, 3000);
+                        setIsLoading(false);
+                        setFormResetDisp(true);
                     }
                 }
             } catch (err: any) {
                 setContent("Please check again");
                 setConfirmTitle("I agree");
-                setIsConfirm(true);
+                setIsModalConfirmDisp(true);
+                setIsLoading(false);
                 setTittle(err?.response?.data?.error || err.message);
             }
         }
         handleConfirm();
     });
+
+    const resetPasswordHandler = () => {
+        // if (res.data) {
+        //     setTittle(res.data.message);
+        //     setContent(
+        //         "You can login right now, you will redirect to login in some minutes..."
+        //     );
+        //     setIsConfirm(true);
+        //     setConfirmTitle("Go to home now");
+        //     setTimeout(() => {
+        //         redirect("/");
+        //     }, 3000);
+        // }
+    }
 
     const confirmToHomePage = () => {
         redirect('/')
@@ -59,13 +70,13 @@ const ConfirmSignUpByLink = () => {
 
     return (
         <>
-            <LoadingComponent isLoading={isConfirm} />
-            {isConfirm && (
+            <LoadingComponent isLoading={isLoading} />
+            {isModalConfirmDisp && (
                 <Dialog
                     onClose={() => {
-                        setIsConfirm(false);
+                        setIsModalConfirmDisp(false);
                     }}
-                    isOpen={isConfirm}
+                    isOpen={isModalConfirmDisp}
                     title={title}
                     message={content}
                     confirmTitle={confirmTitle}
@@ -76,4 +87,4 @@ const ConfirmSignUpByLink = () => {
     );
 };
 
-export default ConfirmSignUpByLink;
+export default ConfirmResetPassword;
