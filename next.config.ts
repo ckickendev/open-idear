206cff0222616591e837b8c458e381025ee291b9
ckickendev <thesoonafu@gmail.com>
2025-04-23 22:03:49 +0700

    config build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from 'next'
- 
+
 const nextConfig: NextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
- 
+
 export default nextConfig
