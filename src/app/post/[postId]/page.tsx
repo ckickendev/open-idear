d3ab07e7ae58d7c1534d56c0c79fbebc90404ab9
gintaka <thesoonafu@gmail.com>
2025-07-17 23:29:13 +0700

    post layout

---
+++ src/app/post/[postId]/page.tsx
@@ -0,0 +1,18 @@
+export default async function PostLists({
+    params
+} : { params: { postId: string } }
+) {
+    const getProduct = async (id: string) => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_ROOT_BACKEND}/post/${id}`, {
+            next: { revalidate: 10 }
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return res.json();
+    };
+
+    // const postData = await getProduct(params.postId);
+
+    return <>Post List {params.postId}</>
+}
